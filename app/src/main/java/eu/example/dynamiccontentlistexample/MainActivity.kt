541e98b05a9fad4d4fb832a57d92713bce6714cf
Michael Christensen <mcct@privat.dk>
2021-11-29 21:00:29 +0100

    Made a button with hardcoded new name When i press button UI is updated

--- app/src/main/java/eu/example/dynamiccontentlistexample/MainActivity.kt
+++ app/src/main/java/eu/example/dynamiccontentlistexample/MainActivity.kt
@@ -1,6 +1,5 @@
 package eu.example.dynamiccontentlistexample
 
-// Video 38
 // Now 42 tested Git branch
 
 import android.os.Bundle
@@ -11,56 +10,62 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import eu.example.dynamiccontentlistexample.ui.theme.DynamicContentListExampleTheme
-
-//
-val namesList: ArrayList<String> = arrayListOf("John", "Michael", "Anna", "Ingrid", "Bente")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingList(names = namesList)
+            MainScreen()
         }
     }
 }
-
 // Example of using kotlin code with Jetpack Composable functions
-// takes a list of names as parameter
+// calls @GreetingList
 @Composable
-fun GreetingList(names: List<String>){
+fun MainScreen(){
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Another way of doing a for loop. Calls @Greeting for each name in the List
-        names.forEach{ Greeting(name = it)}
-
-        Button(onClick = { namesList.add("New name") }) {
-            Text(text = "Add new name")
-    }
-
+        GreetingList() // call @GreetingList
     }
 }
 
 @Composable
+fun GreetingList() {
+    // Remember state when recomposition happens
+    val greetingListState = remember {mutableStateListOf<String>("knud", "Bettine")}
+    for (name in greetingListState){
+        Greeting(name = name)
+    }
+
+    // state is changed when we press button, and recomposition will happen
+    //
+    Button(onClick = { greetingListState.add("New name") }) {
+        Text(text = "Add new name")
+    }
+}
+
+// greet a name
+@Composable
 fun Greeting(name: String) {
     Text(
         text = "Hello $name!",
-        style = MaterialTheme.typography.h4
+        style = MaterialTheme.typography.h5
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    GreetingList(names = namesList)
+    MainScreen()
 
 }
